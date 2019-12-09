6a34c0de7e636d6554a30b9035bc8a46c0041842
catchthecookie <mrsergio39@gmail.com>
2019-12-09 12:06:01 +0100

    service worker from dist

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -11,16 +11,16 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
 importScripts(
-  "/precache-manifest.7616a4cb517eeb419ce4692a2329ff92.js"
+  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
 );
 
-workbox.core.setCacheNameDetails({prefix: "aa-vue"});
+importScripts("/precache-manifest.7616a4cb517eeb419ce4692a2329ff92.js");
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
+workbox.core.setCacheNameDetails({ prefix: "aa-vue" });
+
+self.addEventListener("message", event => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
 });
